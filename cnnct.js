c8073ef571e41211c043c4ea1e460b722e594662
goelrishabh9 <goel.rishabh9@gmail.com>
2019-06-03 18:15:32 +0530

    heya

--- cnnct.js
+++ cnnct.js
@@ -69,7 +69,7 @@ module.exports.getResolvedErrors=function(q,Callback){
 }
 
 module.exports.getUnresolvedErrors=function(Callback){
-	connection.query(`select * from app_feature_error join errors where app_feature_error.error_id=errors.id AND app_feature_error.app_feature_id='${q}'AND app_feature_error.status='UNRESOLVED'`,function(err,data)
+	connection.query(`select * from app_feature_error join errors where app_feature_errors.error_id=errors.id AND app_feature_error.app_feature_id='${q}'AND app_feature_error.status='UNRESOLVED'`,function(err,data)
 	{if (err)
 		console.log(err)
 	else
